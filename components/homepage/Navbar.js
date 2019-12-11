40b030419b5ca27f4bf711d965498325b6c2de0a
PramodShelvant <pramodsheelvant@gmail.com>
2019-12-11 16:25:46 +0530

    1 file modified

--- components/homepage/Navbar.js
+++ components/homepage/Navbar.js
@@ -27,7 +27,7 @@ const navitems=[{
 export default () => 
 <React.Fragment>
 <nav className="navbar navbar-expand-lg navbar-light bg-primary p-0 w-100" >
-  <a className="navbar-brand text-white" href="#">Hospital Management</a>
+  <a className="navbar-brand text-white mx-4" href="#">Hospital Management</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
