5a1624b47ab0f5d24661169b7ff8e5d5ef261771
PramodShelvant <pramodsheelvant@gmail.com>
2019-12-19 17:31:17 +0530

    1 file modified

--- components/Patient/data.js
+++ components/Patient/data.js
@@ -1,16 +1,64 @@
-const sideitems=[
+const Receptionist=[
   {
-    icon:'fa fa-calendar',
-    name:'My Appointment',
+    icon:'fa fa-user-o',
+    name:'Receptionist',
     submenu:[
        {
-    icon:'fa fa-stethoscope',
-    name:'OPD'
+    icon:'fa fa-plus',
+    name:'Add Appointment'
   }, {
-    icon:'fa fa-stethoscope',
-    name:'OPD'
+    icon:'fa fa-phone',
+    name:'Call Log'
+  },
+  {
+    icon:'fa fa-clipboard',
+    name:'Postal'
+  }, {
+    icon:'fa fa-building',
+    name:'Complain'
   }
     ]
+  },
+   {
+    icon:'fa fa-stethoscope',
+    name:'OPD'
+  },
+   {
+    icon:'fa fa-bed',
+    name:'IPD'
+  },
+   {
+    icon:'fa fa-medkit',
+    name:'Pharmacy'
+  },
+   {
+    icon:'fa fa-flask',
+    name:'Pathology'
+  },
+   {
+    icon:'fas fa-microscope',
+    name:'Radiology'
+  },
+   {
+    icon:'fa fa-cut',
+    name:'Operation Theatre'
+  },
+  
+   {
+    icon:'fa fa-ambulance',
+    name:'Ambulance'
+  },
+  
+   {
+    icon:'fa fa-tint',
+    name:'Blood bank'
+  }
+
+];
+const patient=[
+  {
+    icon:'fa fa-user-o',
+    name:'Receptionist',
   },
    {
     icon:'fa fa-stethoscope',
